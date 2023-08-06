066cdea145a33ff3682f3e599def6f6a15e97c7c
andrwondabeat <moralesocampoandres@gmail.com>
2023-08-05 22:03:59 -0300

    First commit

---
+++ index.ts
@@ -0,0 +1,92 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const url = 'https://leagueoflegends.fandom.com/wiki/List_of_champions'
+
+
+
+
+const scrapeChampion = async (page:any, champion:any) => {
+  await page.goto(champion.url, { timeout: 0 })
+
+  const info = await page.evaluate((champ:any) => {
+
+    const scrapeSidebar = (dataSource: string): string[] | string =>{
+      const items: string[] | string = []
+      const parent: HTMLElement  = document.querySelector(`.portable-infobox .pi-item[data-source=${dataSource}] .pi-data-value`)
+      const anchors: HTMLElement[] = Array.from(parent.querySelectorAll('a'))
+
+      if (dataSource === "release"){
+        return anchors[0].innerHTML.split('-')[0]
+      }
+
+      if (dataSource === "resource" ||dataSource === "rangetype" || dataSource === "adaptivetype" || dataSource === "cost"){
+        return anchors[1].innerHTML
+      }
+
+      for (let i = 1 ; i < anchors.length ; i += 2){
+        items.push(anchors[i].innerHTML)
+      }
+      return items
+    }
+
+    const release = scrapeSidebar("release")
+    const classes = scrapeSidebar("legacy")
+    const positions = scrapeSidebar("position")
+    const resource = scrapeSidebar("resource")
+    const rangeType = scrapeSidebar("rangetype")
+    const dmgType = scrapeSidebar("adaptivetype")
+    const price = scrapeSidebar("cost")
+    
+    return {release, classes, positions, resource, rangeType, dmgType, price}
+  })
+  await page.goBack()
+
+  return {
+    ...champion,
+    info,
+  }
+}
+
+const main = async () => {
+
+  const start = Date.now()
+  const browser = await puppeteer.launch({ headless: false, timeout: 0})
+  const page = await browser.newPage()
+  await page.goto(url, {timeout: 0})
+
+  const champions = await page.evaluate(() => {
+
+    let counter = 0
+    const championsElements: HTMLElement[] = Array.from(document.querySelectorAll('tr td span span a'))
+    const championsData: Object = championsElements.map((champ:any, counter) => ({
+
+      id: counter,
+      name: champ.innerHTML.split('<br>')[0],
+      description: champ.innerHTML.split('<br>')[1].charAt(0).toUpperCase() + champ.innerHTML.split('<br>')[1].slice(1),
+      url: champ.href,
+
+    }))
+    
+    counter++
+    return championsData
+  })
+
+  const scrapedData: string[] = []
+
+  for (const champion of champions) {
+    const champ = await scrapeChampion(page, champion)
+    console.log(champ)
+    scrapedData.push(champ)
+  }
+
+  await browser.close()
+  
+  const timeTaken = Math.floor(((Date.now() - start) / 1000) / 60)
+
+  fs.writeFile('champs.json', JSON.stringify(scrapedData), (err:any) =>{
+    if (err) throw err
+    console.log(`JSON done in ${timeTaken} minutes.`)
+  })
+}
+
+main()
